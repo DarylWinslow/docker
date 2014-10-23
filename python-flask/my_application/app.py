6fe73a3fafb384f7f648d599ff4b0d19377cdfe4
DarylWinslow <darylwinslow87@gmail.com>
2014-10-23 17:27:55 +0300

    Hello

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -14,3 +14,30 @@ def upload_file():
     return '',201
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id

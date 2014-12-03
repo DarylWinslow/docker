628a133b102292c6c3a932489fda1a3149421058
DarylWinslow <darylwinslow87@gmail.com>
2014-12-03 21:22:23 +0200

    Yes yes

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -3,6 +3,33 @@ from flask import request
 from os import listdir
 from os.path import isfile, join
 
+def multiples_of_3_or_5():
+        for number in xrange(1000):
+            if not number % 3 or not number % 5:
+                yield number
+
+    print sum(multiples_of_3_or_5())
+
+
+def fib():
+        x,y = 0,1
+        while True:
+            yield x
+            x,y = y, x+y
+
+    def even(seq):
+        for number in seq:
+            if not number % 2:
+                yield number
+
+    def under_a_million(seq):
+        for number in seq:
+            if number > 1000000:
+                break
+            yield number
+
+    print sum(even(under_a_million(fib())))
+
 
 app = Flask(__name__)
 
